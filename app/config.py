bc92c2db4ccca2e32d2970c81870e64dc70bb55b
unknown <muhammadnaviesr.gmail.com>
2025-10-17 13:11:50 +0800

    Pembaruan isi config.py sekaligus pemindahannya

--- app/config.py
+++ app/config.py
@@ -1,17 +1,18 @@
 """ Konfigurasi ENV """
+import logging
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     """ Class Settings untuk konfigurasi ENV """
-    DATABASE_URL: str 
+    DATABASE_URL: str | None = None
     PROJECT_NAME: str = "FastAPI with SQLAlchemy"
     PROJECT_VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     DEBUG: bool = True
 
-    SECRET_KEY: str
+    SECRET_KEY: str | None = None
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  
-    ALGORITHM: str 
+    ALGORITHM: str  = "HS256"
 
 
     
@@ -22,3 +23,10 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 settings = Settings()
+
+if not settings.DATABASE_URL:
+    logging.warning("Variable DATABASE_URL belum di set.    Harap isi di .env untuk produksi.")
+
+if not settings.SECRET_KEY:
+    logging.warning("Variable SECRET_KEY belum di set. Harap isi di .env untuk produksi.")
+
